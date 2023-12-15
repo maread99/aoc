fbdb52fc26240541860e49fd9d4e5e9f213e45fc
Marcus Read <marcusaread@gmail.com>
2023-12-15 11:52:11 +0000

    14_rev change pattern identification method

--- 2023/14_rev.py
+++ 2023/14_rev.py
@@ -4,11 +4,13 @@ Attempt at a quicker version that could get the solution out by iteration.
 Didn't happen, but this one's a a lot quicker. Gets it out in a few
 seconds via identification of the pattern.
 
-EDIT: For the exemplar solution see hyper-neutrino's:
+Rewrote the pattern identification part to recognise that the pattern
+will repeat whenever the arrays' are the same at a specific point of a
+cycle as they were at that point of a previous cycle. This simpler approach
+seems to be the way employed by most solutions.
+
+For an exemplar solution see hyper-neutrino's:
     https://github.com/hyper-neutrino/advent-of-code/blob/main/2023/day14p2.py
-That solution, as most of those that I've seen do, recognise that the pattern
-will repeat whenever the grid's in the same state as it's been in before.
-From there can extrapolate forwards.
 
 #numpy  #matrix  #patterns
 """
@@ -78,52 +80,36 @@ arr = np.flip(arr, axis=0)
 
 # part a
 
-for i, line in enumerate(arr):
-    arr[i] = tilt_line(line)
+arr_a = arr.copy()
+for i, line in enumerate(arr_a):
+    arr_a[i] = tilt_line(line)
 
-locs = np.argwhere(arr == 1)  # locate the rocks
+locs = np.argwhere(arr_a == 1)  # locate the rocks
 load = sum(LENGTH - locs.transpose()[1])
 print(load)
 
 # part b
 
-loads = [load]
-num_unique_loads = [1]
+seen = set()
+arrs = []
 
+CYCLES = 1_000_000_000
+for i in range(0, CYCLES):
+    as_tups = tuple(tuple(line) for line in arr)
+    if as_tups in seen:
+        cycle_idx = i
+        break
+    seen.add(as_tups)
+    arrs.append(as_tups)
+    for _ in range(4):
+        for i, line in enumerate(arr):
+            arr[i] = tilt_line(line)
+        arr = np.flip(arr.transpose(), axis=1)
 
-SAMPLE_TILTS = 1000
-for i in range(1, SAMPLE_TILTS):
-    arr = np.flip(arr.transpose(), axis=1)
-    for i, line in enumerate(arr):
-        arr[i] = tilt_line(line)
-    if not i % 2:
-        locs = np.argwhere(arr == 1)
-    else:
-        locs = np.argwhere(np.flip(arr.transpose()) == 1)
-    loads.append(sum(LENGTH - locs.transpose()[1]))
-    num_unique_loads.append(len(set(loads)))
-
-
-# following repeats code of initial solution, solving by identifying underlying pattern
-
-num_possibles = num_unique_loads[-1]
-length = 2
-# `start` is lowest index when all possible loads were registered
-start = num_unique_loads.index(num_possibles)
-left = loads[start : start + length]
-right = loads[start + length : start + length * 2]
-
-while not (left == right):
-    length += 1
-    left = loads[start : start + length]
-    right = loads[start + length : start + length * 2]
-
-pat_len = len(left)
-# make sure pattern continues to repeat rather than falling within a larger pattern
-for n, (strt, stop) in enumerate(itertools.pairwise(range(start, len(loads), length))):
-    assert loads[strt:stop] == left
-assert n > 4
-
-ROUNDS = 4000000000 - start - 1
-idx = ROUNDS % pat_len
-print(left[idx])
+first_idx = arrs.index(as_tups)
+pat_len = cycle_idx - first_idx
+remainder = (CYCLES - first_idx) % pat_len
+final_tups = arrs[first_idx + remainder]
+locs = np.argwhere(np.array(final_tups) == 1)
+load = sum(LENGTH - locs.transpose()[1])
+print(load)
