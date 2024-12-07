ac193a1b3de509f727aa6f64a816741dfcf7d194
Marcus Read <marcusaread@gmail.com>
2024-12-07 11:02:19 +0000

    07 edit

--- 2024/07.py
+++ 2024/07.py
@@ -5,23 +5,15 @@ I messed around for a while with permutations and combinations of the
 operators before realising that I was after a product with repetition.
 
 part b: 19mins
-Long execution time of 1min 40secs. I assumed that an optimisation of my
-part_a solution would be required and so ditched using `functools.reduce`
-in favour of a custom reduce which breaks whenever the total exceeds the
-sought result. However, this actually makes no meaningful difference
-to the execution time - I could have got the answer out much quicker by
-just using reduce again and letting it run.
+30 second execution time. (NB I've edited this file to correct the number
+of repetitions when evaluating the products - I needed a minus 1 in there.
+With the superfluous repetition execution was taking 1 minute 40 seconds!)
 
 total: 54mins, 14.3x bottom of the leaderboard.
 
-A simple optimisation is to append the lines that fail part_a to a list and
-then only check these in part_b (then adding back the total from part_a).
-However, I found it only saved about 7 seconds, not really significant
-given the total execution time here. I originally solved without this
-optimisation and haven't included it here.
-
-Clearly, there has to be a way to solve this puzzle with a much quicker
-execution...
+NOTE This solution is inefficient! A more efficient implementation is
+recursion which avoids much of the computation duplication. I'll put up
+a revised version.
 
 #reduce #product #brute-force
 """
@@ -33,16 +25,16 @@ from aocd import get_data
 
 raw = get_data(day=7, year=2024)
 
-raw = """190: 10 19
-3267: 81 40 27
-83: 17 5
-156: 15 6
-7290: 6 8 6 15
-161011: 16 10 13
-192: 17 8 14
-21037: 9 7 18 13
-292: 11 6 16 20
-"""
+# raw = """190: 10 19
+# 3267: 81 40 27
+# 83: 17 5
+# 156: 15 6
+# 7290: 6 8 6 15
+# 161011: 16 10 13
+# 192: 17 8 14
+# 21037: 9 7 18 13
+# 292: 11 6 16 20
+# """
 
 lines_raw = raw.splitlines()
 lines = []
@@ -54,14 +46,19 @@ for line in lines_raw:
 
 
 total_a = 0
+failed_lines = []
 for res, values in lines:
-    prods = itertools.product([sum, math.prod], repeat=len(values))
+    failed = True
+    prods = itertools.product([sum, math.prod], repeat=len(values) - 1)
     for p in prods:
         g = (f for f in p)
         total = functools.reduce(lambda *vals: next(g)(vals), values)
         if total == res:
             total_a += total
+            failed = False
             break
+    if failed:
+        failed_lines.append((res, values))
 print(total_a)
 
 
@@ -69,16 +66,29 @@ def combine(vals):
     return int(str(vals[0]) + str(vals[1]))
 
 
-total = 0
-for res, values in lines:
-    prods = itertools.product([sum, math.prod, combine], repeat=len(values))
+total_b = 0
+for res, values in failed_lines:
+    prods = itertools.product([sum, math.prod, combine], repeat=len(values) - 1)
     for p in prods:
-        cum = values[0]
-        for f, b in zip(p, values[1:]):
-            cum = f([cum, b])
-            if cum > res:
-                break
-        if cum == res:
-            total += cum
+        g = (f for f in p)
+        total = functools.reduce(lambda *vals: next(g)(vals), values)
+        if total == res:
+            total_b += total
             break
-print(total)
+print(total_b + total_a)
+
+
+# part_b 'optimisation' which actully offers no improvement over the solution above.
+# total = 0
+# for res, values in lines:
+#     prods = itertools.product([sum, math.prod, combine], repeat=len(values)-1)
+#     for p in prods:
+#         cum = values[0]
+#         for f, b in zip(p, values[1:]):
+#             cum = f([cum, b])
+#             if cum > res:
+#                 break  # fail early if exceed the required value
+#         if cum == res:
+#             total += cum
+#             break
+# print(total)
