2fff184569b43058e4bf84f3179dc0e1c601b61a
Marcus Read <marcusaread@gmail.com>
2024-12-12 10:22:55 +0000

    12

---
+++ 2024/12.py
@@ -0,0 +1,129 @@
+"""Day 12: Garden Groups
+
+part a: 45mins
+Pleased with this time for the first part, although suspect there might have
+been a much quicker way to do it. I was conscious of wanting to get things
+set up as best I could for whatever part b held in store...
+
+part b: 67mins
+Identifies 'sides' with a 2-tuple key where:
+    [0] the direction a bird would look to see the side from just inside
+    the plot perimeter. For example, UP if the side is 'above' the cell
+    immediately inside in the perimeter.
+    [1] the value of the row or column that lies immediately next to the
+    plot (on the outer side of the perimeter) in this direction.
+
+Initially I just counted the number of 'sides' and voila, example answer
+was too low. Didn't realise the error until debugging a specific plot
+revealed this appraoch will count as just 1 any 'side' (as I'd defined it)
+which is not continuous, such as for plot "V" in the example between the
+columns indexed 1 and 2. I thought of two ways to correct for this and went
+first with the wrong one (tried to add on the number of plot locations that
+adjoin the 'side', but of course this only works if the side is broken by a
+single location, as in the "V" region, but not if it's broken by a strip of
+more than one location). Went with the much simpler original idea of, for
+each 'side; adding one each time an otherwise contiguous strip of non-plot
+locations (on the outer side of the perimeter) was broken by one or more
+plot locations).
+
+total: 1hour 52mins, 6.3x bottom of the leaderboard
+Respectable time for me, shame I wasted a bit on part b.
+
+#grid  #complex-numbers  #DFS
+"""
+
+import itertools
+from collections import defaultdict
+
+from aocd import get_data
+
+raw = get_data(day=12, year=2024)
+
+# raw  ="""RRRRIICCFF
+# RRRRIICCCF
+# VVRRRCCFFF
+# VVRCCCJFFF
+# VVVVCJJCFE
+# VVIVCCJJEE
+# VVIIICJJEE
+# MIIIIIJJEE
+# MIIISIJEEE
+# MMMISSJEEE
+# """
+
+GRID = {}
+rows = raw.splitlines()
+for j, r in enumerate(rows):
+    for i, c in enumerate(r):
+        GRID[complex(i, j)] = c
+
+GRID_REV = defaultdict(set)
+for loc, typ in GRID.items():
+    GRID_REV[typ].add(loc)
+
+UP = -1j
+DOWN = 1j
+LEFT = -1
+RIGHT = 1
+DIRS = (UP, DOWN, LEFT, RIGHT)
+
+typs = set(GRID_REV)
+
+plots_all: dict[str, list[list[set[complex]]]] = {}  # type ref to list of plots
+while typs:
+    typ = typs.pop()
+    typ_locs = GRID_REV[typ]
+    plots: list[set[complex]] = []  # list of plot
+    while typ_locs:
+        stk = [typ_locs.pop()]
+        plot: set[complex] = set([stk[0]])  # plot as set of locations
+        while stk:
+            loc = stk.pop()
+            for d in DIRS:
+                nloc = loc + d
+                if nloc in typ_locs:
+                    stk.append(nloc)
+                    typ_locs.remove(nloc)
+                    plot.add(nloc)
+        plots.append(plot)
+    plots_all[typ] = plots
+
+tot = 0
+for typ, plots in plots_all.items():
+    for p in plots:
+        area = len(p)
+        prm = 0
+        for loc in p:
+            for d in DIRS:
+                # consider as forming 1 length of perimeter every non-plot cell that's
+                # adjacent to a plot cell
+                if loc + d not in p:
+                    prm += 1
+        tot += area * prm
+
+print(tot)
+
+
+tot = 0
+for typ, plots in plots_all.items():
+    for p in plots:
+        area = len(p)
+        # `sides` is a mapping of 'side' to corresponding cells on outer side of
+        # perimeter, where 'side' is as defined in module doc notes for part b
+        sides = defaultdict(list)
+        for loc in p:
+            for d in DIRS:
+                if (nloc := loc + d) not in p:
+                    side = (d, nloc.imag) if d in (UP, DOWN) else (d, nloc.real)
+                    sides[side].append(nloc)
+        prm = len(sides)
+        # correct `prm` for those 'sides' that are broken by the plot itself, and hence
+        # comprise two or more actual sides.
+        for (d, row_col), locs in sides.items():
+            # vals as column or row values of cells along side (on outer side of perimeter)
+            vals = sorted([loc.real if d in (UP, DOWN) else loc.imag for loc in locs])
+            # add one actual side every time these cells do not for contiguous regions
+            prm += sum((1 for a, b in itertools.pairwise(vals) if b - a != 1))
+        tot += area * prm
+
+print(tot)
--- 2024/README.md
+++ 2024/README.md
@@ -6,17 +6,17 @@ See the [main README](https://github.com/maread99/aoc) for notes on how I go abo
 
 ### Summary of key features of solutions
 
-[1](./01.py) #lists &nbsp;#sort &nbsp;#zip  
+[1](./01.py) #arrays &nbsp;#sort &nbsp;#zip  
 
-[2](./02.py) #lists  
+[2](./02.py) #arrays  
 
 [3](./03.py) #strings  
 [3_alt](./03_alt.py) #regex  
 
 [4](./04.py) navigate a #grid with #complex-numbers  
 
-[5](./05.py) #lists  
-[5_alt](./05_alt.py) #sort with a comparison function. Also #lists &nbsp;#compare  
+[5](./05.py) #arrays  
+[5_alt](./05_alt.py) #sort with a comparison function. Also #arrays &nbsp;#compare  
 
 [6](./06.py) #sets &nbsp;#complex-numbers  
 [6_rev](./06_alt.py) further optimised. #sets &nbsp;#complex-numbers  
@@ -24,13 +24,15 @@ See the [main README](https://github.com/maread99/aoc) for notes on how I go abo
 [7](./07.py) #reduce &nbsp;#product &nbsp;#brute-force  
 [7_rev](./07_rev.py) #recursion &nbsp;#brute-force
 
-[8](./08.py) #complex-numbers to represent #vectors. Also #sets &nbsp;#combinations  
+[8](./08.py) #complex-numbers to represent #vectors. &nbsp;Also #sets &nbsp;#combinations  
 
 [9](./09.py) manipulation of #arrays  
 [9_alt](./09_alt.py) manipulation of #strings  
 
-[10](./10.py) #DFS  #recursion  #sets. Also navigating a #grid with #complex-numbers  
+[10](./10.py) #DFS &nbsp;#recursion &nbsp;#sets. &nbsp;Also navigating a #grid with #complex-numbers  
 [10_alt](./10_alt.py) #stack instead of recursion  
 
 [11](./11.py) #counter &nbsp;#codify-rules  
 [11_alt](./11_alt.py) #recursion &nbsp;#memoization &nbsp;#codify-rules  
+
+[12](./12.py) #DFS. &nbsp;Also navigating a #grid with #complex-numbers  
